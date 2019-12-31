31c7c33ededcdd6e65dab293b8e75f3a1d3c311e
AtheeshRathnaweera27 <rathnaweeraatheesh72@gmail.com>
2020-01-01 02:46:33 +0530

    add new card creation and instruction notification

--- Draggable cards/assets/script.js
+++ Draggable cards/assets/script.js
@@ -4,7 +4,6 @@ var newCardFormDisplay = false;
 
 $(document).ready(function () {
 
-
     var allCards = document.getElementsByClassName("noteCard");
 
     console.log("document ready function started. " + allCards.length);
@@ -15,45 +14,29 @@ $(document).ready(function () {
 //draggable js
 document.onmousedown = function (ev) {
     var id = ev.target.id;
+    
+    console.log("mouse donw: "+id);
 
     if (id != null) {
-        dragElement(document.getElementById(id).parentElement);
+        if (id.includes("card")) {//drag only cards
+           dragElement(document.getElementById(id).parentElement);
+        }
+
         //get the selected note card div
     }
 }
 
-function displayAddNewForm() {
-    console.log("add new form btn clicked.");
-    var newCardForm = document.getElementsByClassName("addNewForm")[0];
-
-    var offSetTopOfNewBtn = document.getElementsByClassName("addNewCardBtn")[0].offsetTop;
-
-    console.log("off set to new btn : " + offSetTopOfNewBtn);
-
-//    newCardForm.style.position = "absolute";
-//    newCardForm.style.top = "14vh";
-//    newCardForm.style.right = "10px";
-
-    if (newCardFormDisplay) {
-        newCardForm.style.left = "100vw";
-        newCardFormDisplay= false;
-    } else {
-        newCardForm.style.left = "72vw";
-        newCardFormDisplay = true;
-    }
-
-
-
-
-}
-
 function dragElement(elmnt) {
+    console.log("drag started.");
     var pos1 = 0,
         pos2 = 0,
         pos3 = 0,
         pos4 = 0;
 
     elmnt.addEventListener("mousedown", function (e) {
+        
+        console.log("element mouse down adding");
+        
         e = e || window.event;
         e.preventDefault();
 
@@ -61,10 +44,10 @@ function dragElement(elmnt) {
         pos3 = e.clientX;
         pos4 = e.clientY;
 
-        //add mouse release function to the selected notecard
+        //add mouse release function to the document
         document.onmouseup = closeDragElement;
 
-        //add mouse move function to the selected notecard
+        //add mouse move function to the document
         document.onmousemove = elementDrag;
     });
 
@@ -90,13 +73,69 @@ function dragElement(elmnt) {
 //draggable js
 
 //add new card click event
-function addNewCard() {
-    console.log("add new card clicked.");
+function displayHowToIns(){
+    var instructionCard = document.getElementsByClassName("instructionCard")[0];
+    
+    instructionCard.style.right = "62%";
+    
+     setTimeout(function(){
+         console.log("timr up");
+         instructionCard.style.right = "100vw";
+     },4000);
+   // instructionCard.style.right = "100vw";
+    
+}
 
-    var newCardItem = '<div class="noteCard"><div class="noteCardHeader" id="card' + (cardsAmount + 1) + '"><input value="Put your title here" class="cardInput"/></div><div class="noteCardBody">Put your content here</div></div>';
+
+function displayAddNewForm() {
+    var newCardForm = document.getElementsByClassName("addNewForm")[0];
+
+    var offSetTopOfNewBtn = document.getElementsByClassName("addNewCardBtn")[0].offsetTop;
+
+    if (newCardFormDisplay) {
+        newCardForm.style.left = "100vw";
+        newCardFormDisplay = false;
+    } else {
+        newCardForm.style.left = "72vw";
+        newCardFormDisplay = true;
+    }
+
+}
+
+
+function saveCard() {
+
+    var noteTitle = document.getElementsByClassName("titleInput")[0].value;
+    var noteContent = document.getElementsByClassName("contentInput")[0].value;
+
+    if (noteTitle.length > 0 && noteContent.length > 0) {
+        //valid inputs
+        addNewCard(noteTitle, noteContent);
+    }
+}
+
+function addNewCard(title, content) {
+
+    var newCardItem = '<div class="noteCard"><div class="noteCardHeader" id="card' + (cardsAmount + 1) + '">' + title + '</div><div class="noteCardBody">' + content + '</div></div>';//html string of not card
 
     document.getElementsByClassName("canvas")[0].innerHTML += newCardItem;
+    
+    var newlyCreatedCard = document.getElementById("card"+(cardsAmount+1)).parentElement;//get the parent element of newly created note card
+    
+    displayTheNewCardInRandomPosition(newlyCreatedCard);
 
-    cardsAmount += 1;
+    cardsAmount += 1; //increase the cards amount
+
+    document.getElementsByClassName("addNewCardBtn")[0].click();//hide the form
 }
 //add new card click event
+
+function displayTheNewCardInRandomPosition(element){
+    //generate random number and set the position
+    var randomValue =  Math.floor(Math.random() * 80 ) + 1;
+    console.log("random : "+randomValue);
+    
+    element.style.position = "absolute";
+    element.style.top = randomValue + "vh";
+    element.style.left = randomValue + "vw";
+}
