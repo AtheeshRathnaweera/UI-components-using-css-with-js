84058a3b7fd74676a07c7d35b506b430141abead
AtheeshRathnaweera27 <rathnaweeraatheesh72@gmail.com>
2020-01-16 02:38:35 +0530

    drawing board color change,undo,redo,canvas restore completed

--- Drawing Canvas/assets/Script/script.js
+++ Drawing Canvas/assets/Script/script.js
@@ -5,18 +5,33 @@ var startedDrawing = false;
 var canvasTopOffSet = 0;
 var canvasLeftOffSet = 0;
 var pickedColor = "#181726";
+var boardColor = "#FFFFFF";
+
+var strokeHistoryData = []; //store stroke history data
+var currentStroke = null; //hold the currently drawing stroke data -->{boardColor: white,startX: ev.pageX - canvasLeftOffSet, startY: ev.pageY -canvasTopOffSet,strokeColor: pickedColor,cords: []}
+var removedStrokeHistory = []; //use for undo and redo
+
+var canvasCleaned = false;
 
 $(document).ready(function () {
 
     setCanvasDimensions();
-    pickedColor = "#"+document.getElementsByClassName("jscolor")[0].value;
+    pickedColor = "#" + document.getElementById("penColor").value;
+    boardColor = "#" + document.getElementById("boardColor").value;
 
-    console.log("ready started. " + canvasTopOffSet + " " + canvasLeftOffSet+" "+pickedColor);
+    drawTheDrawingBoard(0, 0, myCanvas.width, myCanvas.height, boardColor);
+
 });
 
 window.onresize = function () {
     console.log("resized");
+
     setCanvasDimensions();
+
+    if (!cleanCanvas) {
+        printHistory();
+    }
+
 }
 
 
@@ -29,18 +44,63 @@ myCanvas.onmousedown = function (ev) {
     }
 
     startedDrawing = true;
-    context.moveTo(ev.pageX - canvasLeftOffSet, ev.pageY - canvasTopOffSet);
+
+    setContextToStartNewStroke(ev.pageX - canvasLeftOffSet, ev.pageY - canvasTopOffSet);
+
+    var tempStroke = {
+        startX: ev.pageX - canvasLeftOffSet,
+        startY: ev.pageY - canvasTopOffSet,
+        strokeColor: pickedColor,
+        cords: []
+    };
+
+    currentStroke = tempStroke;
 }
 
 myCanvas.onmouseup = function (ev) {
     console.log("mouse up " + ev.pageX + " " + ev.pageY);
     startedDrawing = false;
-    //ctx.closePath();
+
+    strokeHistoryData.push(currentStroke);
+    currentStroke = null;
 }
 
 myCanvas.onmousemove = function (e) {
     console.log("mouse move " + e.pageX + " " + e.pageY);
-    drawOnCanvas(e.pageX, e.pageY);
+
+    if (startedDrawing) {
+        drawOnCanvas(e.pageX, e.pageY, pickedColor);
+        currentStroke.cords.push({
+            x: e.pageX,
+            y: e.pageY
+        });
+    }
+
+}
+
+function printHistory() {
+    console.log("history size : " + strokeHistoryData.length);
+
+    drawTheDrawingBoard(0, 0, myCanvas.width, myCanvas.height, boardColor);
+
+    for (var i = 0; i < strokeHistoryData.length; i++) {
+        setContextToStartNewStroke(strokeHistoryData[i].startX, strokeHistoryData[i].startY);
+
+        var strokeCords = strokeHistoryData[i].cords;
+        var strokeColor = strokeHistoryData[i].strokeColor;
+
+        for (var c = 0; c < strokeCords.length; c++) {
+            drawOnCanvas(strokeCords[c].x, strokeCords[c].y, strokeColor);
+        }
+
+        console.log(strokeHistoryData[i].startX + " " + strokeHistoryData[i].startY + " " + strokeHistoryData[i].cords);
+    }
+
+}
+
+function setContextToStartNewStroke(xCord, yCord) {
+    context.beginPath();
+    context.moveTo(xCord, yCord);
 }
 
 function setCanvasDimensions() {
@@ -51,42 +111,85 @@ function setCanvasDimensions() {
     canvasLeftOffSet = myCanvas.offsetLeft;
 }
 
-function drawOnCanvas(xCordinates, yCordniates) {
+function drawTheDrawingBoard(startX, startY, endX, endY, recBoardColor) {
+    context.beginPath();
+    context.fillStyle = recBoardColor;
+    context.fillRect(startX, startY, endX, endY);
 
-    if (startedDrawing) {
-        context.lineTo(xCordinates - canvasLeftOffSet, yCordniates - canvasTopOffSet);
-        context.strokeStyle = pickedColor;
-        context.stroke();
+}
+
+function drawOnCanvas(xCordinates, yCordniates, strokeColor) {
+
+    context.lineTo(xCordinates - canvasLeftOffSet, yCordniates - canvasTopOffSet);
+    context.strokeStyle = strokeColor;
+    context.stroke();
+
+}
+
+//tool actions
+function undoDrawing() {
+
+    if (!canvasCleaned && strokeHistoryData.length > 0) {
+        removedStrokeHistory.push(strokeHistoryData.pop());
+        printHistory();
+    }
+
+}
+
+function redoDrawing() {
+
+    if (!canvasCleaned && removedStrokeHistory.length > 0) {
+        strokeHistoryData.push(removedStrokeHistory.pop());
+        printHistory();
     }
 
 }
 
 function cleanCanvas() {
-    context.save();
     context.clearRect(0, 0, context.canvas.width, context.canvas.height);
     context.beginPath();
+
+    canvasCleaned = true;
 
 }
 
 function restoreCanvas() {
-    context.restore();
-}
+    printHistory();
+    canvasCleaned = false;
 
-function changeColor(hexCode) {
-    console.log("this is the picked color: " + hexCode);
-    pickedColor = "#" + hexCode;
-    context.beginPath();
 }
 
 function printCanvas() {
     $("#canvasimg").attr("src", $("#myCanvas").get(0).toDataURL("img/jpg"));
 
     $("#printedResultsDisplayModal").modal();
+}
+//tool actions
+
+//picker actions
+function changeColor(hexCode) {
+    //change the drawing pen color
+    pickedColor = "#" + hexCode;
+    context.beginPath();
+}
+
+function changeBoardColor(hexCode) {
+    //change the board color
+    boardColor = "#" + hexCode;
+
+    if (printCanvas) {
+        drawTheDrawingBoard(0, 0, myCanvas.width, myCanvas.height, boardColor);
+    } else {
+        printHistory();
+    }
 
 
 }
+//picker actions
 
+//download button in print modal ---> download the creeated image
 function downloadPrintedResult() {
+
     var fileName = document.getElementById("imageName").value;
 
     if (fileName == "") {
@@ -94,10 +197,11 @@ function downloadPrintedResult() {
     }
 
     var url = document.getElementById("canvasimg").src.replace(/^data:image\/[^;]+/, 'data:application/octet-stream');
-    
+
     var downloadLink = document.getElementById("imageDownloadLink");
-    
+
     downloadLink.href = url;
     downloadLink.download = fileName;
     downloadLink.click();
 }
+//download button in print modal ---> download the creeated image
